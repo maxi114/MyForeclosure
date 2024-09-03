58afb9b7b51b8b54ca24beb600b2bb04c3b7c8d0
maximillian kodi <mokodi114@gmail.com>
2024-09-03 19:43:14 -0400

    added navigation on dashboard

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,9 +1,10 @@
-
+import DashNav from "@/components/dashboard/navigation/DashNav"
 
 export default function DashboardLayout({ children }) {
   return (
     <html lang="en">
         <body>
+          <DashNav/>
             {children}
         </body>
     </html>
