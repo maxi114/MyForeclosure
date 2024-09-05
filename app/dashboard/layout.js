11b5eb7792a75349060e1f6ca15a3aed220208e1
Jerry <99816957+Mr-Higgs@users.noreply.github.com>
2024-09-05 11:39:56 -0400

    Dashboard Layout UI and theme

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,11 +1,26 @@
+'use client';
 
+import { useAuthContext, useThemeContext } from '../context';
+import { ThemeSettings } from '../context/ThemeContext';
+import VerticalLayout from '@/layouts/Vertical';
+import HorizontalLayout from '@/layouts/Horizontal';
+import { redirect } from 'next/navigation';
 
 export default function DashboardLayout({ children }) {
+  const { settings } = useThemeContext();
+  const { user } = useAuthContext();
+
+  if (!user) {
+    redirect('/account/login');
+  }
+
+  const Layout = settings.layout.type === ThemeSettings.layout.type.vertical
+    ? VerticalLayout
+    : HorizontalLayout;
+
   return (
-    <html lang="en">
-        <body>
-            {children}
-        </body>
-    </html>
+    <Layout>
+      {children}
+    </Layout>
   );
 }
---
+++ app/utils/menu.js
@@ -0,0 +1,30 @@
+import { MENU_ITEMS } from '@/lib/menu-items';
+
+export const getMenuItems = () => {
+  // NOTE - You can fetch from server and return here as well
+  return MENU_ITEMS;
+};
+
+export const findAllParent = (menuItems, menuItem) => {
+  let parents = [];
+  const parent = findMenuItem(menuItems, menuItem['parentKey']);
+
+  if (parent) {
+    parents.push(parent['key']);
+    if (parent['parentKey']) parents = [...parents, ...findAllParent(menuItems, parent)];
+  }
+  return parents;
+};
+
+export const findMenuItem = (menuItems, menuItemKey) => {
+  if (menuItems && menuItemKey) {
+    for (let i = 0; i < menuItems.length; i++) {
+      if (menuItems[i].key === menuItemKey) {
+        return menuItems[i];
+      }
+      const found = findMenuItem(menuItems[i].children, menuItemKey);
+      if (found) return found;
+    }
+  }
+  return null;
+};
